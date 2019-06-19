7dbd8587efa3441dcfc1e5b241f33237ce3b865c
Noah <noahbehtel@gmail.com>
2019-06-19 17:03:20 -0500

    new colors

--- client/components/myProjects.js
+++ client/components/myProjects.js
@@ -20,72 +20,40 @@ class MyProjects extends Component {
     return (
       <div className='multicomp'>
         <div className='title'>Some Things I've Been Working On</div>
-        {this.state.mobile ? (
-          <div className='tiles-mobile'>
-            {projects.map(p => {
-              if (p.name === this.state.selected) {
-                return (
-                  <a href={p.url} className='singleProject' key={p.url}>
-                    <div>
-                      <img src={p.img} className='productImage' />
-                      <div className='projectCap-selected'>{p.desc}</div>
-                      <div className='projectCapBack'>{'    '}</div>
-                    </div>
-                  </a>
-                )
-              }
+
+        <div className='tiles'>
+          {projects.map(p => {
+            if (p.name === this.state.selected) {
               return (
-                <div
-                  className='singleProject'
-                  key={p.url}
-                  onClick={() => {
-                    this.setState({ selected: p.name })
-                  }}
-                >
+                <div className='singleProject' key={p.url}>
                   <div>
-                    <div className='projectCap'>{p.name}</div>
-                    <div className='projectCapBack'>{'    '}</div>
                     <img src={p.img} className='productImage' />
+                    <a href={p.url} className='launchbody'>
+                      <div className='launch'>Launch</div>
+                    </a>
+                    <div className='projectCap-selected'>{p.desc}</div>
+                    <div className='projectCapBack'>{'    '}</div>
                   </div>
                 </div>
               )
-            })}
-          </div>
-        ) : (
-          <div className='tiles'>
-            {projects.map(p => {
-              if (p.name === this.state.selected) {
-                return (
-                  <div className='singleProject' key={p.url}>
-                    <div>
-                      <img src={p.img} className='productImage' />
-                      <a href={p.url} className='launchbody'>
-                        <div className='launch'>Launch</div>
-                      </a>
-                      <div className='projectCap-selected'>{p.desc}</div>
-                      <div className='projectCapBack'>{'    '}</div>
-                    </div>
-                  </div>
-                )
-              }
-              return (
-                <div
-                  className='singleProject'
-                  key={p.url}
-                  onClick={() => {
-                    this.setState({ selected: p.name })
-                  }}
-                >
-                  <div>
-                    <div className='projectCap'>{p.name}</div>
-                    <div className='projectCapBack'>{'    '}</div>
-                    <img src={p.img} className='productImage' />
-                  </div>
+            }
+            return (
+              <div
+                className='singleProject'
+                key={p.url}
+                onClick={() => {
+                  this.setState({ selected: p.name })
+                }}
+              >
+                <div>
+                  <div className='projectCap'>{p.name}</div>
+                  <div className='projectCapBack'>{'    '}</div>
+                  <img src={p.img} className='productImage' />
                 </div>
-              )
-            })}
-          </div>
-        )}
+              </div>
+            )
+          })}
+        </div>
       </div>
     )
   }
